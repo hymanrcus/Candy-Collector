754a2a1501f91af37e247ebd4e8efb7cfdec5092
Marcus <hymanmarcus95@gmail.com>
2022-10-02 12:54:26 -0400

    add show page for candies

--- controllers/candies.js
+++ controllers/candies.js
@@ -1,9 +1,33 @@
 import { Candy } from '../models/candy.js'
 
 function index(req, res) {
-  console.log("Gucci Bag")
+  Candy.find({})
+  .then(candies => {
+    res.render('candies/index', {
+      candies,
+      title: "Candies"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+	req.body.tasty = !!req.body.tasty
+  Candy.create(req.body)
+  .then(candy => {
+    res.redirect('/candies')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/candies')
+  })
 }
 
 export {
-  index
+  index,
+  create
 }
--- routes/candies.js
+++ routes/candies.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import * as candiesCtrl from '../controllers/candies.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET localhost:3000/bags
 router.get('/', candiesCtrl.index)
+router.post('/', isLoggedIn, candiesCtrl.create)
 
 export {
   router
