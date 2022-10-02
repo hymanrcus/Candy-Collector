bffce42ef984ff1929f09e832f574d598092bd01
Marcus <hymanmarcus95@gmail.com>
2022-10-02 14:01:48 -0400

    add update functionality

--- controllers/candies.js
+++ controllers/candies.js
@@ -1,49 +1,71 @@
-import { Candy } from '../models/candy.js'
+import { Candy } from "../models/candy.js";
 
 function index(req, res) {
   Candy.find({})
-  .then(candies => {
-    res.render('candies/index', {
-      candies,
-      title: "Candies"
+    .then((candies) => {
+      res.render("candies/index", {
+        candies,
+        title: "Candies",
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect("/")
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
 }
 
 function create(req, res) {
-  req.body.owner = req.user.profile._id
-	req.body.tasty = !!req.body.tasty
+  req.body.owner = req.user.profile._id;
+  req.body.tasty = !!req.body.tasty;
   Candy.create(req.body)
-  .then(candy => {
-    res.redirect('/candies')
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/candies')
-  })
+    .then((candy) => {
+      res.redirect("/candies");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/candies");
+    });
 }
 
 function show(req, res) {
   Candy.findById(req.params.id)
-  .populate("owner")
-  .then(candy => {
-    res.render('candies/show', {
-      candy,
-      title: "Candy Details"
+    .populate("owner")
+    .then((candy) => {
+      res.render("candies/show", {
+        candy,
+        title: "Candy Details",
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/candies')
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/candies");
+    });
 }
 
-export {
-  index,
-  create,
-  show,
+function edit(req, res) {
+  Candy.findById(req.params.id)
+    .then((candy) => {
+      res.render("candies/edit", {
+        candy,
+        title: "Edit Candy",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
 }
+
+function update(req, res) {
+  Candy.findByIdAndUpdate(req.params.id, req.body)
+    .then(candy => {
+      console.log("candy", candy);
+      res.redirect(`/candies/${candy._id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect(`/candies`);
+    });
+}
+
+export { index, create, show, edit, update };
--- routes/candies.js
+++ routes/candies.js
@@ -7,6 +7,8 @@ const router = Router()
 // GET localhost:3000/bags
 router.get('/', candiesCtrl.index)
 router.get('/:id', candiesCtrl.show)
+router.put('/:id',isLoggedIn, candiesCtrl.update)
+router.get('/:id/edit', candiesCtrl.edit)
 router.post('/', isLoggedIn, candiesCtrl.create)
 
 export {
